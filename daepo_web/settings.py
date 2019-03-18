64a14a49c25f054e33d5ae7af5c929cea7d52475
c_hwanch <c_hwanch@qti.qualcomm.com>
2019-03-18 10:19:12 +0900

    update db setting

--- daepo_web/settings.py
+++ daepo_web/settings.py
@@ -136,3 +136,7 @@ CHANNEL_LAYERS = {
     },
 }
 ASGI_APPLICATION = 'daepo_web.routing.application'
+
+import dj_database_url
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
